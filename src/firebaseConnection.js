9dc7018dd5aee9d25e695342ddfab7ce26e0b9fa
Felipe Alves <felipeaamodio@gmail.com>
2022-01-19 20:46:18 -0300

    instalado o firebase conforme recomendado na documentação

---
+++ src/firebaseConnection.js
@@ -0,0 +1,22 @@
+//conexão com o firebase
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyCTUbcfaOj4fOvc2ycP4tyljN-T1JF8OMY",
+    authDomain: "meuapp-bc4a5.firebaseapp.com",
+    databaseURL: "https://meuapp-bc4a5-default-rtdb.firebaseio.com",
+    projectId: "meuapp-bc4a5",
+    storageBucket: "meuapp-bc4a5.appspot.com",
+    messagingSenderId: "633815856860",
+    appId: "1:633815856860:web:d41e9e80dab35d09336619",
+    measurementId: "G-7KYFHD9Q0J"
+  };
+  
+  // Initialize Firebase
+  if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+  }
+//const app = initializeApp(firebaseConfig);
+
+export default firebase;
